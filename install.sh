25fa77f0ac45ff5114598e372a71274f11d6f9ca
mazerty <mazerty@gmail.com>
2017-09-18 17:32:32 +0200

    better syntax

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
-sudo wget -O /usr/local/bin/idem.py https://raw.githubusercontent.com/mazerty/idem/master/idem.py
+sudo wget https://raw.githubusercontent.com/mazerty/idem/master/idem.py -O /usr/local/bin/idem.py
 sudo chmod +x /usr/local/bin/idem.py
