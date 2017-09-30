f312c8c4b1a5dacedf4a31e4d01bc4d0288df7ec
mazerty <mazerty@gmail.com>
2017-09-30 15:53:01 +0200

    migrated existing scripts to new root usage + deleted old scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-sudo wget https://raw.githubusercontent.com/mazerty/idem/master/idem.py -O /usr/local/bin/idem.py
-sudo wget https://raw.githubusercontent.com/mazerty/idem/master/template.py -O /usr/local/bin/template.py
-sudo chmod +x /usr/local/bin/*.py
+wget https://raw.githubusercontent.com/mazerty/idem/master/idem.py -O /usr/local/bin/idem.py
+wget https://raw.githubusercontent.com/mazerty/idem/master/template.py -O /usr/local/bin/template.py
+chmod +x /usr/local/bin/*.py
