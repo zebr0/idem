5bcb5fba4e55261f6ddc12866c804d6dc4f20f7c
mazerty <mazerty@gmail.com>
2017-12-27 14:55:30 +0100

    the config file is now optional, an example will be in the readme

--- install.sh
+++ install.sh
@@ -2,4 +2,3 @@
 wget https://raw.githubusercontent.com/mazerty/idem/master/idem.py -O /usr/local/bin/idem.py
 wget https://raw.githubusercontent.com/mazerty/idem/master/template.py -O /usr/local/bin/template.py
 chmod +x /usr/local/bin/*.py
-wget https://raw.githubusercontent.com/mazerty/idem/master/idem.conf -O /etc/idem.conf
