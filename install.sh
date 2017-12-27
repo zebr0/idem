f8aab1df458ced85c41ce20363a9ed5e5da1a0ca
mazerty <mazerty@gmail.com>
2017-12-27 15:02:03 +0100

    template is now its own project: templ

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/sh -e
 wget https://raw.githubusercontent.com/mazerty/idem/master/idem.py -O /usr/local/bin/idem.py
-wget https://raw.githubusercontent.com/mazerty/idem/master/template.py -O /usr/local/bin/template.py
 chmod +x /usr/local/bin/*.py
