b741b72f8b42df8c8fa081bcfb51847ee156e7c3
mazerty <mazerty@gmail.com>
2018-01-21 20:26:25 +0100

    test suite

---
+++ test/test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -ex
+
+# starts the mock server
+cd mock
+python3 -m http.server &
+pid=$!
+sleep 1
+cd ..
+
+# creates tmp directory
+mkdir tmp
+
+# test dry output before run
+sudo ../src/idem run test --dry > tmp/dry_before
+diff tmp/dry_before results/dry_before
+
+# test first run
+sudo ../src/idem run test > tmp/first_run
+diff tmp/first_run results/first_run
+diff tmp/idem/f4fc192a9bb717d392c21bda92b930ba results/idem/f4fc192a9bb717d392c21bda92b930ba
+diff tmp/idem/55802db65c9fe6f2e052fb91b56f3249 results/idem/55802db65c9fe6f2e052fb91b56f3249
+diff tmp/idem/de6b9dad18c35dbf9108396facfcf879 results/idem/de6b9dad18c35dbf9108396facfcf879
+diff tmp/resource mock/resources/include/resource
+
+# test dry output after run
+sudo ../src/idem run test --dry > tmp/dry_after
+diff tmp/dry_after results/dry_after
+
+# test second run
+sudo ../src/idem run test > tmp/second_run
+diff tmp/second_run results/second_run
+
+# stops the mock server
+kill ${pid}
+
+# cleans tmp directory
+sudo rm -rf tmp
