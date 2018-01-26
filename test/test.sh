b3c508c38555ed8bdb6874f55e59e4563a5387b6
mazerty <mazerty@gmail.com>
2018-01-26 16:34:11 +0100

    added a retry mode

--- test/test.sh
+++ test/test.sh
@@ -15,11 +15,13 @@ sudo ../src/idem run test --dry > tmp/dry_before
 diff tmp/dry_before results/dry_before
 
 # test first run
-sudo ../src/idem run test > tmp/first_run
+sudo ../src/idem run test --retry > tmp/first_run
 diff tmp/first_run results/first_run
 diff tmp/idem/f4fc192a9bb717d392c21bda92b930ba results/idem/f4fc192a9bb717d392c21bda92b930ba
 diff tmp/idem/55802db65c9fe6f2e052fb91b56f3249 results/idem/55802db65c9fe6f2e052fb91b56f3249
 diff tmp/idem/de6b9dad18c35dbf9108396facfcf879 results/idem/de6b9dad18c35dbf9108396facfcf879
+diff tmp/idem/2e319d8e37e1a0293ab8680413aba8b1 results/idem/2e319d8e37e1a0293ab8680413aba8b1
+diff tmp/idem/33d97341f7e26ce567dce8c078d26f74 results/idem/33d97341f7e26ce567dce8c078d26f74
 diff tmp/resource mock/resources/include/resource
 
 # test dry output after run
