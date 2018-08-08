6122ba009ada05dc88703d4580c85cd1da99c0c2
mazerty <mazerty@gmail.com>
2018-08-08 10:53:09 +0200

    rework: renamed idem to zebr0-script

--- test/test.sh
+++ test/test.sh
@@ -18,22 +18,22 @@ cd ..
 sudo zebr0-init -u http://localhost:8000 -p dummy_project -s dummy_stage
 
 # test dry output before run
-sudo ../src/idem -d tmp/idem run test --dry > tmp/dry_before
+sudo ../src/zebr0-script -d tmp/history run test --dry > tmp/dry_before
 diff tmp/dry_before results/dry_before
 
 # test first run
-sudo ../src/idem -d tmp/idem run test --retry > tmp/first_run
+sudo ../src/zebr0-script -d tmp/history run test --retry > tmp/first_run
 diff tmp/first_run results/first_run
-diff tmp/idem/f4fc192a9bb717d392c21bda92b930ba results/idem/f4fc192a9bb717d392c21bda92b930ba
-diff tmp/idem/2e319d8e37e1a0293ab8680413aba8b1 results/idem/2e319d8e37e1a0293ab8680413aba8b1
-diff tmp/idem/33d97341f7e26ce567dce8c078d26f74 results/idem/33d97341f7e26ce567dce8c078d26f74
+diff tmp/history/f4fc192a9bb717d392c21bda92b930ba results/history/f4fc192a9bb717d392c21bda92b930ba
+diff tmp/history/2e319d8e37e1a0293ab8680413aba8b1 results/history/2e319d8e37e1a0293ab8680413aba8b1
+diff tmp/history/33d97341f7e26ce567dce8c078d26f74 results/history/33d97341f7e26ce567dce8c078d26f74
 
 # test dry output after run
-sudo ../src/idem -d tmp/idem run test --dry > tmp/dry_after
+sudo ../src/zebr0-script -d tmp/history run test --dry > tmp/dry_after
 diff tmp/dry_after results/dry_after
 
 # test second run
-sudo ../src/idem -d tmp/idem run test > tmp/second_run
+sudo ../src/zebr0-script -d tmp/history run test > tmp/second_run
 diff tmp/second_run results/second_run
 
 # stops the mock server
