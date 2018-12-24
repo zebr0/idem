16c9cae157e59bd4e33c84b3b13ade1c78e215ef
Thomas JOUANNOT <mazerty@gmail.com>
2018-12-24 07:20:22 +0100

    added error checking tests

--- test/test.sh
+++ test/test.sh
@@ -18,11 +18,11 @@ cd ..
 zebr0-init -c tmp -u http://localhost:8000 -p dummy_project -s dummy_stage
 
 # test dry output before run
-sudo ../src/zebr0-script -c tmp -d tmp/history run test --dry > tmp/dry_before
+sudo ../src/zebr0-script -c tmp -d tmp/history run test-ok --dry > tmp/dry_before
 diff tmp/dry_before results/dry_before
 
 # test first run
-sudo ../src/zebr0-script -c tmp -d tmp/history run test > tmp/first_run
+sudo ../src/zebr0-script -c tmp -d tmp/history run test-ok > tmp/first_run
 diff tmp/first_run results/first_run
 diff tmp/history/f4fc192a9bb717d392c21bda92b930ba results/history/f4fc192a9bb717d392c21bda92b930ba
 diff tmp/history/2e319d8e37e1a0293ab8680413aba8b1 results/history/2e319d8e37e1a0293ab8680413aba8b1
@@ -31,12 +31,20 @@ diff tmp/history/55802db65c9fe6f2e052fb91b56f3249 results/history/55802db65c9fe6
 diff tmp/lookup mock/lookup
 
 # test dry output after run
-sudo ../src/zebr0-script -c tmp -d tmp/history run test --dry > tmp/dry_after
+sudo ../src/zebr0-script -c tmp -d tmp/history run test-ok --dry > tmp/dry_after
 diff tmp/dry_after results/dry_after
 
 # test second run
-sudo ../src/zebr0-script -c tmp -d tmp/history run test > tmp/second_run
+sudo ../src/zebr0-script -c tmp -d tmp/history run test-ok > tmp/second_run
 diff tmp/second_run results/second_run
+
+# test script ko
+sudo ../src/zebr0-script -c tmp -d tmp/history run test-ko-script > tmp/ko-script || true
+diff tmp/ko-script results/ko-script
+
+# test lookup ko
+sudo ../src/zebr0-script -c tmp -d tmp/history run test-ko-lookup > tmp/ko-lookup || true
+diff tmp/ko-lookup results/ko-lookup
 
 # stops the mock server
 kill $(cat tmp/pid) && rm tmp/pid
