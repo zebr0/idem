8893445c54592adbb7f23abd72afb85a4e5a0744
Thomas JOUANNOT <mazerty@gmail.com>
2019-01-28 20:26:39 +0100

    rework to yaml and json for scripts + back to oop

---
+++ test/test-json.sh
@@ -0,0 +1,61 @@
+#!/bin/sh -ex
+
+# cleans a potentially failed previous test run
+[ -f tmp/pid ] && [ -d /proc/$(cat tmp/pid) ] && kill $(cat tmp/pid)
+[ -d tmp/ ] && rm -rf tmp/
+
+# creates tmp directory
+mkdir tmp
+
+# starts the mock server
+cd mock-json
+python3 -m http.server &
+echo $! > ../tmp/pid
+sleep 1
+cd ..
+
+# initializes zebr0 configuration
+zebr0-init -c tmp -u http://localhost:8000 -p dummy_project -s dummy_stage
+
+# test dry output before run
+../src/zebr0-script -c tmp -d tmp/history run test-ok --dry > tmp/dry_before
+diff tmp/dry_before results/dry_before
+
+# test first run
+../src/zebr0-script -c tmp -d tmp/history run test-ok > tmp/first_run
+diff tmp/first_run results/first_run
+diff tmp/history/2e319d8e37e1a0293ab8680413aba8b1 results/history/2e319d8e37e1a0293ab8680413aba8b1
+diff tmp/history/33d97341f7e26ce567dce8c078d26f74 results/history/33d97341f7e26ce567dce8c078d26f74
+diff tmp/history/55802db65c9fe6f2e052fb91b56f3249 results/history/55802db65c9fe6f2e052fb91b56f3249
+diff tmp/history/80ce453923ebe1a927fcd3e31541c977 results/history/80ce453923ebe1a927fcd3e31541c977
+diff tmp/history/e2e406ee41e39a849d0f307143ff2e67 results/history/e2e406ee41e39a849d0f307143ff2e67
+diff tmp/history/f4fc192a9bb717d392c21bda92b930ba results/history/f4fc192a9bb717d392c21bda92b930ba
+diff tmp/lookup mock-json/lookup
+
+# test dry output after run
+../src/zebr0-script -c tmp -d tmp/history run test-ok --dry > tmp/dry_after
+diff tmp/dry_after results/dry_after
+
+# test second run
+../src/zebr0-script -c tmp -d tmp/history run test-ok > tmp/second_run
+diff tmp/second_run results/second_run
+
+# test script ko
+../src/zebr0-script -c tmp -d tmp/history run test-ko-script > tmp/ko-script || true
+diff tmp/ko-script results/ko-script
+
+# test lookup ko
+../src/zebr0-script -c tmp -d tmp/history run test-ko-lookup > tmp/ko-lookup || true
+diff tmp/ko-lookup results/ko-lookup
+
+# stops the mock server in a few seconds, to make the following test fail
+echo "sleep 2 && kill $(cat tmp/pid) && rm tmp/pid" | at now
+
+# test connection ko
+../src/zebr0-script -c tmp -d tmp/history run test-ko-connection > tmp/ko-connection-dirty || true
+sed "s/0x.*>/0xFFFF>/g" tmp/ko-connection-dirty > tmp/ko-connection-clean
+diff tmp/ko-connection-clean results/ko-connection-clean
+diff tmp/history/33ea634aeb8bb8ba6c648f7d1cb42538 results/history/33ea634aeb8bb8ba6c648f7d1cb42538
+
+# cleans tmp directory
+rm -rf tmp
--- test/test-yaml.sh
+++ test/test-yaml.sh
@@ -8,7 +8,7 @@
 mkdir tmp
 
 # starts the mock server
-cd mock
+cd mock-yaml
 python3 -m http.server &
 echo $! > ../tmp/pid
 sleep 1
@@ -24,12 +24,13 @@ diff tmp/dry_before results/dry_before
 # test first run
 ../src/zebr0-script -c tmp -d tmp/history run test-ok > tmp/first_run
 diff tmp/first_run results/first_run
-diff tmp/history/f4fc192a9bb717d392c21bda92b930ba results/history/f4fc192a9bb717d392c21bda92b930ba
 diff tmp/history/2e319d8e37e1a0293ab8680413aba8b1 results/history/2e319d8e37e1a0293ab8680413aba8b1
 diff tmp/history/33d97341f7e26ce567dce8c078d26f74 results/history/33d97341f7e26ce567dce8c078d26f74
 diff tmp/history/55802db65c9fe6f2e052fb91b56f3249 results/history/55802db65c9fe6f2e052fb91b56f3249
-diff tmp/history/c11bd38dad44c0074dbb5b7ea6a9e8ae results/history/c11bd38dad44c0074dbb5b7ea6a9e8ae
-diff tmp/lookup mock/lookup
+diff tmp/history/80ce453923ebe1a927fcd3e31541c977 results/history/80ce453923ebe1a927fcd3e31541c977
+diff tmp/history/e2e406ee41e39a849d0f307143ff2e67 results/history/e2e406ee41e39a849d0f307143ff2e67
+diff tmp/history/f4fc192a9bb717d392c21bda92b930ba results/history/f4fc192a9bb717d392c21bda92b930ba
+diff tmp/lookup mock-yaml/lookup
 
 # test dry output after run
 ../src/zebr0-script -c tmp -d tmp/history run test-ok --dry > tmp/dry_after
@@ -54,6 +55,7 @@ echo "sleep 2 && kill $(cat tmp/pid) && rm tmp/pid" | at now
 ../src/zebr0-script -c tmp -d tmp/history run test-ko-connection > tmp/ko-connection-dirty || true
 sed "s/0x.*>/0xFFFF>/g" tmp/ko-connection-dirty > tmp/ko-connection-clean
 diff tmp/ko-connection-clean results/ko-connection-clean
+diff tmp/history/33ea634aeb8bb8ba6c648f7d1cb42538 results/history/33ea634aeb8bb8ba6c648f7d1cb42538
 
 # cleans tmp directory
 rm -rf tmp
