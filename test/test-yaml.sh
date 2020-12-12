bc3b672fbfd8200cfb7323fdeebcefa5510db36d
mazerty <mazerty@gmail.com>
2020-12-12 20:33:20 +0100

    moved script to root directory

--- test/test-yaml.sh
+++ test/test-yaml.sh
@@ -18,11 +18,11 @@ cd ..
 zebr0-init -c tmp -u http://localhost:8000 -p dummy_project -s dummy_stage
 
 # test dry output before run
-../src/zebr0-script -c tmp -d tmp/history run test-ok --dry > tmp/dry_before
+../zebr0-script -c tmp -d tmp/history run test-ok --dry > tmp/dry_before
 diff tmp/dry_before results/dry_before
 
 # test first run
-../src/zebr0-script -c tmp -d tmp/history run test-ok > tmp/first_run
+../zebr0-script -c tmp -d tmp/history run test-ok > tmp/first_run
 diff tmp/first_run results/first_run
 diff tmp/history/2e319d8e37e1a0293ab8680413aba8b1 results/history/2e319d8e37e1a0293ab8680413aba8b1
 diff tmp/history/33d97341f7e26ce567dce8c078d26f74 results/history/33d97341f7e26ce567dce8c078d26f74
@@ -33,26 +33,26 @@ diff tmp/history/f4fc192a9bb717d392c21bda92b930ba results/history/f4fc192a9bb717
 diff tmp/dir/lookup mock-yaml/lookup
 
 # test dry output after run
-../src/zebr0-script -c tmp -d tmp/history run test-ok --dry > tmp/dry_after
+../zebr0-script -c tmp -d tmp/history run test-ok --dry > tmp/dry_after
 diff tmp/dry_after results/dry_after
 
 # test second run
-../src/zebr0-script -c tmp -d tmp/history run test-ok > tmp/second_run
+../zebr0-script -c tmp -d tmp/history run test-ok > tmp/second_run
 diff tmp/second_run results/second_run
 
 # test script ko
-../src/zebr0-script -c tmp -d tmp/history run test-ko-script > tmp/ko-script || true
+../zebr0-script -c tmp -d tmp/history run test-ko-script > tmp/ko-script || true
 diff tmp/ko-script results/ko-script
 
 # test lookup ko
-../src/zebr0-script -c tmp -d tmp/history run test-ko-lookup > tmp/ko-lookup || true
+../zebr0-script -c tmp -d tmp/history run test-ko-lookup > tmp/ko-lookup || true
 diff tmp/ko-lookup results/ko-lookup
 
 # stops the mock server in a few seconds, to make the following test fail
 echo "sleep 2 && kill $(cat tmp/pid) && rm tmp/pid" | at now
 
 # test connection ko
-../src/zebr0-script -c tmp -d tmp/history run test-ko-connection > tmp/ko-connection-dirty || true
+../zebr0-script -c tmp -d tmp/history run test-ko-connection > tmp/ko-connection-dirty || true
 sed "s/0x.*>/0xFFFF>/g" tmp/ko-connection-dirty > tmp/ko-connection-clean
 diff tmp/ko-connection-clean results/ko-connection-clean
 diff tmp/history/33ea634aeb8bb8ba6c648f7d1cb42538 results/history/33ea634aeb8bb8ba6c648f7d1cb42538
