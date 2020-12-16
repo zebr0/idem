aa939d2da49937b896bf1039cd8b637a4b9b4617
mazerty <mazerty@gmail.com>
2020-12-16 10:03:37 +0100

    migrated test for recursive_lookup

--- tests/test_bli.py
+++ tests/test_bli.py
@@ -76,3 +76,22 @@ def test_lookup(server, capsys):
         assert testfile.read_text() == "dummy\n"
         assert historyfile.read_text() == str(sdfsdf)
         assert capsys.readouterr().out == "done\n"
+
+
+def test_recursive_lookup2(server, capsys):
+    server.data = {"script": ["install package xxx",
+                              {"lookup": "configuration-file", "path": "/etc/xxx/conf.ini"},
+                              "chmod 400 /etc/xxx/conf.ini",
+                              {"include": "second-script"},
+                              {"make-coffee": "black"}],
+                   "second-script": ["install package yyy",
+                                     "yyy configure network"]}
+
+    client = zebr0.Client("http://localhost:8000", configuration_file=Path(""))
+    result = zebr0_script.recursive_lookup2("script", Path("/tmp"), client)
+    assert list(result) == [("install package xxx", Path("/tmp/edd85cad01d197aa80d9edcbfce9a575")),
+                            ({"lookup": "configuration-file", "path": "/etc/xxx/conf.ini"}, Path("/tmp/b61788ba0623fc4d9114699ab00d8bf7")),
+                            ("chmod 400 /etc/xxx/conf.ini", Path("/tmp/df6218e3bf04bcfe1670d1009b08dcbf")),
+                            ("install package yyy", Path("/tmp/fdeb9fb70de466a3975f25725c897913")),
+                            ("yyy configure network", Path("/tmp/c39b670739e8ece59c0f53b6b1b0dfb3"))]
+    assert capsys.readouterr().out == "unknown command, ignored: {'make-coffee': 'black'}\n"
