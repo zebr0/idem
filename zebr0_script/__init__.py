8560d83a65cca73ef3175e8c01488a6d38a3e25f
mazerty <mazerty@gmail.com>
2020-12-29 12:31:32 +0100

    reordered functions

--- zebr0_script/__init__.py
+++ zebr0_script/__init__.py
@@ -17,20 +17,105 @@ KEY = "key"
 TARGET = "target"
 
 
-def log(reports_path: Path, **_) -> None:
+def recursive_fetch_script(client: zebr0.Client, key: str, reports_path: Path) -> Iterator[Tuple[Any, Path]]:
     """
-    Prints a chronologically ordered list of the report files and their content.
+    Fetches a script from the key-value server, validates the tasks' structure and fetches the "include" tasks recursively.
 
+    Beware that malformed tasks are ignored.
+    Also, "key not found" and "key is not a proper yaml or json list" errors are non-blocking in "include" tasks.
+
+    :param client: zebr0 Client instance to the key-value server
+    :param key: key of the script to look for
     :param reports_path: Path to the reports' directory
+    :return: the script's valid tasks and the corresponding report Paths
     """
 
-    def get_mtime(path):
-        return path.stat().st_mtime
+    value = client.get(key)
+    if not value:
+        print(f"key '{key}' not found on server {client.url}")
+    else:
+        tasks = yaml.load(value, Loader=yaml.BaseLoader)
+        if not isinstance(tasks, list):
+            print(f"key '{key}' on server {client.url} is not a proper yaml or json list")
+        else:
+            for task in tasks:
+                if isinstance(task, dict) and task.keys() == {INCLUDE}:
+                    yield from recursive_fetch_script(client, task.get(INCLUDE), reports_path)
+                elif isinstance(task, str) or isinstance(task, dict) and task.keys() == {KEY, TARGET}:
+                    md5 = hashlib.md5(json.dumps(task).encode(zebr0.ENCODING)).hexdigest()
+                    yield task, reports_path.joinpath(md5)
+                else:
+                    print("malformed task, ignored:", json.dumps(task))
 
-    if reports_path.exists():
-        for file in filter(lambda p: p.is_file(), sorted(reports_path.iterdir(), key=get_mtime)):
-            strformat = datetime.datetime.fromtimestamp(get_mtime(file)).strftime("%c")
-            print(file.name, strformat, file.read_text(encoding=zebr0.ENCODING).strip())
+
+def show(url: str, levels: Optional[List[str]], cache: int, configuration_file: Path, reports_path: Path, key: str, **_) -> None:
+    """
+    Fetches a script from the key-value server and displays its tasks along with their current status, whether they have already been executed or not.
+
+    :param url: (zebr0) URL of the key-value server, defaults to https://hub.zebr0.io
+    :param levels: (zebr0) levels of specialization (e.g. ["mattermost", "production"] for a <project>/<environment>/<key> structure), defaults to []
+    :param cache: (zebr0) in seconds, the duration of the cache of http responses, defaults to 300 seconds
+    :param configuration_file: (zebr0) path to the configuration file, defaults to /etc/zebr0.conf for a system-wide configuration
+    :param reports_path: Path to the reports' directory
+    :param key: key of the script to look for
+    """
+
+    client = zebr0.Client(url, levels, cache, configuration_file)
+    for task, report_path in recursive_fetch_script(client, key, reports_path):
+        print("todo:" if not report_path.exists() else "done:", json.dumps(task))
+
+
+def execute(command: str, attempts: int = ATTEMPTS_DEFAULT, pause: float = PAUSE_DEFAULT) -> dict:
+    """
+    Executes a command with the system's shell.
+    Several attempts will be made in case of failure, to cover for e.g. network issues.
+    Standard output will be shown and returned as a list of strings if successful.
+    Beware that dynamic output like the use of carriage return in progress bars won't be rendered properly.
+
+    :param command: command to execute
+    :param attempts: maximum number of attempts before being actually considered a failure
+    :param pause: delay in seconds between two attempts
+    :return: if successful, an execution report as a dictionary
+    """
+
+    for attempt in reversed(range(attempts)):  # [attempts-1 .. 0]
+        sp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, universal_newlines=True, encoding=zebr0.ENCODING)
+
+        stdout = []
+        for line in sp.stdout:
+            line = line.strip()
+            print(line)
+            stdout.append(line)
+
+        if sp.wait() == 0:  # if successful (i.e. the return code is 0)
+            return {"command": command, "stdout": stdout}
+        elif attempt != 0:
+            print(f"failed, {attempt} attempts remaining, will try again in {pause} seconds")
+            time.sleep(pause)
+
+
+def fetch_to_disk(client: zebr0.Client, key: str, target: str) -> dict:
+    """
+    Fetches a key from the key-value server and writes its value into a target file.
+
+    :param client: zebr0 Client instance to the key-value server
+    :param key: key to look for
+    :param target: path to the target file
+    :return: if successful, an execution report as a dictionary
+    """
+
+    value = client.get(key, strip=False)
+    if not value:
+        print(f"key '{key}' not found on server {client.url}")
+    else:
+        try:
+            target_path = Path(target)
+            target_path.parent.mkdir(parents=True, exist_ok=True)  # make sure the parent directories exist
+            target_path.write_text(value, encoding=zebr0.ENCODING)
+
+            return {KEY: key, TARGET: target}
+        except OSError as error:
+            print(error)
 
 
 def run(url: str, levels: Optional[List[str]], cache: int, configuration_file: Path, reports_path: Path, key: str, attempts: int = ATTEMPTS_DEFAULT, pause: float = PAUSE_DEFAULT, **_) -> None:
@@ -73,105 +158,20 @@ def run(url: str, levels: Optional[List[str]], cache: int, configuration_file: P
                 break
 
 
-def show(url: str, levels: Optional[List[str]], cache: int, configuration_file: Path, reports_path: Path, key: str, **_) -> None:
+def log(reports_path: Path, **_) -> None:
     """
-    Fetches a script from the key-value server and displays its tasks along with their current status, whether they have already been executed or not.
+    Prints a chronologically ordered list of the report files and their content.
 
-    :param url: (zebr0) URL of the key-value server, defaults to https://hub.zebr0.io
-    :param levels: (zebr0) levels of specialization (e.g. ["mattermost", "production"] for a <project>/<environment>/<key> structure), defaults to []
-    :param cache: (zebr0) in seconds, the duration of the cache of http responses, defaults to 300 seconds
-    :param configuration_file: (zebr0) path to the configuration file, defaults to /etc/zebr0.conf for a system-wide configuration
     :param reports_path: Path to the reports' directory
-    :param key: key of the script to look for
     """
 
-    client = zebr0.Client(url, levels, cache, configuration_file)
-    for task, report_path in recursive_fetch_script(client, key, reports_path):
-        print("todo:" if not report_path.exists() else "done:", json.dumps(task))
+    def get_mtime(path):
+        return path.stat().st_mtime
 
-
-def recursive_fetch_script(client: zebr0.Client, key: str, reports_path: Path) -> Iterator[Tuple[Any, Path]]:
-    """
-    Fetches a script from the key-value server, validates the tasks' structure and fetches the "include" tasks recursively.
-
-    Beware that malformed tasks are ignored.
-    Also, "key not found" and "key is not a proper yaml or json list" errors are non-blocking in "include" tasks.
-
-    :param client: zebr0 Client instance to the key-value server
-    :param key: key of the script to look for
-    :param reports_path: Path to the reports' directory
-    :return: the script's valid tasks and the corresponding report Paths
-    """
-
-    value = client.get(key)
-    if not value:
-        print(f"key '{key}' not found on server {client.url}")
-    else:
-        tasks = yaml.load(value, Loader=yaml.BaseLoader)
-        if not isinstance(tasks, list):
-            print(f"key '{key}' on server {client.url} is not a proper yaml or json list")
-        else:
-            for task in tasks:
-                if isinstance(task, dict) and task.keys() == {INCLUDE}:
-                    yield from recursive_fetch_script(client, task.get(INCLUDE), reports_path)
-                elif isinstance(task, str) or isinstance(task, dict) and task.keys() == {KEY, TARGET}:
-                    md5 = hashlib.md5(json.dumps(task).encode(zebr0.ENCODING)).hexdigest()
-                    yield task, reports_path.joinpath(md5)
-                else:
-                    print("malformed task, ignored:", json.dumps(task))
-
-
-def fetch_to_disk(client: zebr0.Client, key: str, target: str) -> dict:
-    """
-    Fetches a key from the key-value server and writes its value into a target file.
-
-    :param client: zebr0 Client instance to the key-value server
-    :param key: key to look for
-    :param target: path to the target file
-    :return: if successful, an execution report as a dictionary
-    """
-
-    value = client.get(key, strip=False)
-    if not value:
-        print(f"key '{key}' not found on server {client.url}")
-    else:
-        try:
-            target_path = Path(target)
-            target_path.parent.mkdir(parents=True, exist_ok=True)  # make sure the parent directories exist
-            target_path.write_text(value, encoding=zebr0.ENCODING)
-
-            return {KEY: key, TARGET: target}
-        except OSError as error:
-            print(error)
-
-
-def execute(command: str, attempts: int = ATTEMPTS_DEFAULT, pause: float = PAUSE_DEFAULT) -> dict:
-    """
-    Executes a command with the system's shell.
-    Several attempts will be made in case of failure, to cover for e.g. network issues.
-    Standard output will be shown and returned as a list of strings if successful.
-    Beware that dynamic output like the use of carriage return in progress bars won't be rendered properly.
-
-    :param command: command to execute
-    :param attempts: maximum number of attempts before being actually considered a failure
-    :param pause: delay in seconds between two attempts
-    :return: if successful, an execution report as a dictionary
-    """
-
-    for attempt in reversed(range(attempts)):  # [attempts-1 .. 0]
-        sp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, universal_newlines=True, encoding=zebr0.ENCODING)
-
-        stdout = []
-        for line in sp.stdout:
-            line = line.strip()
-            print(line)
-            stdout.append(line)
-
-        if sp.wait() == 0:  # if successful (i.e. the return code is 0)
-            return {"command": command, "stdout": stdout}
-        elif attempt != 0:
-            print(f"failed, {attempt} attempts remaining, will try again in {pause} seconds")
-            time.sleep(pause)
+    if reports_path.exists():
+        for file in filter(lambda p: p.is_file(), sorted(reports_path.iterdir(), key=get_mtime)):
+            strformat = datetime.datetime.fromtimestamp(get_mtime(file)).strftime("%c")
+            print(file.name, strformat, file.read_text(encoding=zebr0.ENCODING).strip())
 
 
 def main(args: Optional[List[str]] = None) -> None:
